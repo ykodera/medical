31432a601134f974538166a8bcc442cce045026e
ykodera <yk.infotech45@gmail.com>
2019-01-04 12:40:34 +0900

    101行目のconnfdをsockに変更

--- server/test.py
+++ server/test.py
@@ -29,7 +29,7 @@ def main():
     host = "192.168.24.10"
     port = 50000
     elements_number = 10
-    bufsize = 1024
+    bufsize = 4096
     writeenable = False
     listenserver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     readfds = set([listenserver_socket])
@@ -52,11 +52,16 @@ def main():
 
     #例外処理
     try:
-        listenserver_socket.bind((socket.gethostname(), port))
+        #listenserver_socket.bind((socket.gethostname(), port))
+        listenserver_socket.bind((str(host), port))
+    except OSError as e:
+        errortime =  datetime.now()
+        output(log, str(errortime),str(e))
+
+    else:
         listenserver_socket.listen(elements_number)
-
         output(log, 'Waiting for a client to call...','');
-
+    try:
         while True:
             rready_socket, wready_socket, xready_socket = select.select(readfds, [], [])
 
@@ -75,104 +80,110 @@ def main():
 
                 #新規でなければ
                 else:
-                    #データ受信
-                    msg = sock.recv(bufsize)
-                    cfd = sock.fileno()
-                    #print("cfd:")
-                    #print(cfd)
-                    receiveddata_time = datetime.now()
-
-                    #受け取ったデータ長が0だったら
-                    if len(msg) == 0:
-                        del saved_data[sock.fileno()]
-                        readfds.remove(sock)
-                        sock.close()
-
-                        output(log, 'Disconnection: ',str(connfd))
-                        disconnectiontime = datetime.now()
-                        output(log, 'Disconnectiontime at: ', str(disconnectiontime))
+                    try:
+                        #データ受信
+                        msg = sock.recv(bufsize)
+                    except ConnectionResetError as e:
+                        errortime =  datetime.now()
+                        output(log, str(errortime),str(e))
                     else:
-                        received_data = msg.decode('utf-8')
-                        #debug用
-                        if (flag):
-                            debugname = (new_dir_path+'/debug.csv')
-                            debugf = open(debugname, 'a')
-                            flag=False
-                        debugf.write(received_data)
+                        cfd = sock.fileno()
+                        #print("cfd:")
+                        #print(cfd)
+                        receiveddata_time = datetime.now()
 
-                        #output(log, str(receiveddata_time)+ ': ', received_data)
-                        #print("****************")
+                        #受け取ったデータ長が0だったら
+                        if len(msg) == 0:
+                            del saved_data[sock.fileno()]
+                            readfds.remove(sock)
+                            sock.close()
 
-                        #print(msg)
-                        #print("utf-data: "+ received_data)
-                        #print("****************")
-
-                        if received_data == START:
-                            if writeenable:
-                                output(log, 'The file is already open','')
-                                sock.send(b'The file is already open'+ b"\n")
-                                endsession(f, filename, sock, log)
-                            sessionStart_time = datetime.now()
-                            filename = (new_dir_path+'/{0:%Y%m%d_%H%M%S}'.format(sessionStart_time) + '.csv')
-                            #sessionlogname = (new_dir_path+'/{0:%Y%m%d_%H%M%S}'.format(sessionStart_time) + '.log')
-                            #sessionlog = open (sessionlogname, 'a')
-                            output(log, 'Starting the Session at ', str(sessionStart_time))
-                            #output(sessionlog, 'Starting the Session at ', str(sessionStart_time))
-                            sock.send(b'Starting the Session at ' + str(sessionStart_time).encode()+b'\n')
-
-                            f = open (filename, 'a')
-                            output(log, 'Opened a new file: ', filename)
-                            #output(sessionlog, 'Opened a new file: ', filename)
-
-                            sock.send(b'Opened a new file: ' + filename.encode()+b'\n\n')
-
-                            writeenable = True #書き込み可能に
-
-                        elif received_data == QUIT:
-                            if writeenable:
-                                endsession(f, filename, sock, log)
-                                writeenable = False
-
-                        elif received_data == SIGNAL:
-                            #to be conntinued...
-                            calc()
-
-                        elif writeenable:
-                            #received_data:str saved_data:配列
-                            print("saved_data{0}".format(cfd))
-                            print(saved_data[cfd])
-                            tail_data = ''.join(saved_data[cfd])
-                            received_data = tail_data + received_data
-
-                            if(received_data[-1]=='\n'):
-                                print(received_data)
-                                f.write(received_data)
-                                log.write(received_data)
-                                #sessionlog.write(spritdata[i]+'\n')
-
-                                #saved_dataを初期化
-                                saved_data[cfd] = []
+                            output(log, 'Disconnection: ',str(sock))
+                            disconnectiontime = datetime.now()
+                            output(log, 'Disconnectiontime at: ', str(disconnectiontime))
+                        else:
+                            try:
+                                #received_data = msg.decode('utf-8')
+                                received_data = msg.decode('ascii')
+                            except UnicodeDecodeError as e:
+                                errortime =  datetime.now()
+                                output(log, str(errortime),str(e))
                             else:
-                                print('aaaa')
+                                if (flag):
+                                    debugname = (new_dir_path+'/debug.csv')
+                                    debugf = open(debugname, 'a')
+                                    flag=False
+                                #debugf.write(str(received_data))
+                                debugf.write(received_data)
 
-                                splitdata = received_data.split('\n')
+                                #output(log, str(receiveddata_time)+ ': ', received_data)
+                                #print("****************")
+                                #print(msg)
+                                #print("utf-data: "+ received_data)
+                                #print("****************")
 
-                                i=0
-                                while(i < len(splitdata)-1):
+                                if received_data == START:
+                                    if writeenable:
+                                        output(log, 'The file is already open','')
+                                        sock.send(b'The file is already open'+ b"\n")
+                                        endsession(f, filename, sock, log)
+                                    sessionStart_time = datetime.now()
+                                    filename = (new_dir_path+'/{0:%Y%m%d_%H%M%S}'.format(sessionStart_time) + '.csv')
+                                    #sessionlogname = (new_dir_path+'/{0:%Y%m%d_%H%M%S}'.format(sessionStart_time) + '.log')
+                                    #sessionlog = open (sessionlogname, 'a')
+                                    output(log, 'Starting the Session at ', str(sessionStart_time))
+                                    #output(sessionlog, 'Starting the Session at ', str(sessionStart_time))
+                                    sock.send(b'Starting the Session at ' + str(sessionStart_time).encode()+b'\n')
 
-                                    print(splitdata[i])
-                                    log.write(splitdata[i]+'\n')
-                                    #sessionlog.write(spritdata[i]+'\n')
-                                    f.write(splitdata[i]+'\n')
-                                    i=i+1
+                                    f = open (filename, 'a')
+                                    output(log, 'Opened a new file: ', filename)
+                                    #output(sessionlog, 'Opened a new file: ', filename)
+                                    sock.send(b'Opened a new file: ' + filename.encode()+b'\n\n')
 
-                                string=splitdata[i]
-                                saved_data[cfd]=[]
+                                    writeenable = True #書き込み可能に
 
-                                for c in string:
-                                    saved_data[cfd].append(c)
+                                elif received_data == QUIT:
+                                    if writeenable:
+                                        endsession(f, filename, sock, log)
+                                        writeenable = False
 
+                                elif received_data == SIGNAL:
+                                    #to be conntinued...
+                                    Calc()
 
+                                elif writeenable:
+                                    #received_data:str saved_data:配列
+                                    #print("saved_data{0}".format(cfd))
+                                    #print(saved_data[cfd])
+                                    tail_data = ''.join(saved_data[cfd])
+                                    received_data = tail_data + received_data
+
+                                    if(received_data[-1]=='\n'):
+                                        print(received_data)
+                                        f.write(received_data)
+                                        log.write(received_data)
+                                        #sessionlog.write(spritdata[i]+'\n')
+
+                                        #saved_dataを初期化
+                                        saved_data[cfd] = []
+                                    else:
+
+                                        splitdata = received_data.split('\n')
+
+                                        i=0
+                                        while(i < len(splitdata)-1):
+
+                                            print(splitdata[i])
+                                            log.write(splitdata[i]+'\n')
+                                            #sessionlog.write(spritdata[i]+'\n')
+                                            f.write(splitdata[i]+'\n')
+                                            i=i+1
+
+                                        string=splitdata[i]
+                                        saved_data[cfd]=[]
+
+                                        for c in string:
+                                            saved_data[cfd].append(c)
 
         f.close()
     #    sessionlog.close()
@@ -183,11 +194,11 @@ def main():
         for sock in readfds:
             sock.close()
         log.close()
+        print("終了しました")
     return
 
-
-    #計算箇所(予定),計算の前の加工もどうするか考えないと...
+    #未実装
 def calc():
-    print('Calc!')
+    print('to be continue...')
 
 main()
